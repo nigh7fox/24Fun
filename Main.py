846c2a851d7c2f8049d8f9bb0d72161003116fc7
Joshua Tromp <joshwow91@gmail@github.com>
2019-04-14 00:15:33 +0200

    ok

--- Main.py
+++ Main.py
@@ -1,11 +1,31 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+def get_data():
+    base_link = 'http://24ora.com/category/local/suceso/'
+    url_request = urllib.request.Request(base_link, headers = {'User-Agent': 'Mozilla/5.0'})
+    url_html = urllib.request.urlopen(url_request).read()
+    return url_html
+
+
+def get_comments():
+    soup = BeautifulSoup(get_data(), 'html.parser')
+    suceso_links = []
+    suceso_articles = []
+    suceso_main = soup.find('div', {'class': 'td-ss-main-content'})
+    for y in suceso_main:
+        suceso_links.append(y.find('a')['href'])
+    for x in suceso_links:
+        print(x)
+    pass 
+
+
 def main():
+    get_comments()
     return 0
 
 
-def get_date():
-    return 0
-
-
-if __name__ == "__name__":
+if __name__ == "__main__":
     main()
 
