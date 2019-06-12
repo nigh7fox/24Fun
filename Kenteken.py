b878251a9d901561bee4a28246798d6699ab6d4a
Joshua Tromp <joshwow91@gmail@github.com>
2019-06-12 18:00:38 +0200

    almost done

--- Kenteken.py
+++ Kenteken.py
@@ -1,23 +1,42 @@
 from collections import Counter
 import urllib.request
 from bs4 import BeautifulSoup
+import csv
+
 
 base_link = 'https://www.rdwdata.nl/kenteken/78GSP6'
+keywords = ['Merk', 'Handelsbenaming', 'Voertuigssoort', 'Brandstof', 'Bouwjaar'] 
+
+
 def get_html():
     url_request = urllib.request.Request(base_link, headers = {'User-Agent': 'Mozilla/5.0'})
     url_html = urllib.request.urlopen(url_request).read()
     return url_html
 
+
 def get_info():
     soup = BeautifulSoup(get_html(),'html.parser')
     kenteken_body = soup.find('div', {'class' : 'data small'})
-    table_list = []
-    for types in kenteken_body:
-            if type(types.find('table')) is not int and :
-                    table_list.append(types.find('table'))
+    all_tds = soup.find_all('td')
+    keyword_list = []
+    keyword_index = []
+    kenteken_data = []
+    for i, x in enumerate(all_tds):
+        if x.string in keywords:
+            keyword_index.append(i)
+    for y in keyword_index:
+        kenteken_data.append(all_tds[y+2].string)
+    return list(set(kenteken_data))
 
-    for i in table_list:
-            if i is not None:
-                    print (i[i.find('td>'):])
-    
-get_info()
+
+def write_to_csv():
+    try:
+        with open('kenteken_data.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(get_info())
+    except FileNotFoundError:
+        print('ERRROOORRRRR REEEEE')
+    finally:
+        f.close()
+
+write_to_csv()
