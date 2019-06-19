abc743ba91ce0d57e0a38f5ba979247450c64165
Raiodomo <rainierkock@gmail.com>
2019-06-19 15:20:42 +0200

    Input for filename of numberplates

--- Kenteken.py
+++ Kenteken.py
@@ -41,7 +41,7 @@ def write_to_csv(a_link):
 def main():
     base_link = 'https://www.rdwdata.nl/kenteken/'  
 
-    fname = 'Kenteken_test.txt'
+    fname = input('Enter filename')
     
     with open(fname) as f:
         for line in f:
