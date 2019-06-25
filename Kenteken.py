61678e7ee51e99c5ccccc8b8edd6523a9f44141b
Raiodomo <rainierkock@gmail.com>
2019-06-25 11:47:02 +0200

    Added comments, changed variable names for readability and changed print statements for readiblity

--- Kenteken.py
+++ Kenteken.py
@@ -5,35 +5,47 @@ import csv
 from bs4 import BeautifulSoup
 from ordered_set import OrderedSet
 
-keywords = ['Merk', 'Handelsbenaming', 'Inrichting', 'Brandstof', 'Vermogen', 'Bouwjaar', 'BPM'] 
+#Words that you want the value of
+keywords = ['Merk', 'Handelsbenaming', 'Inrichting', 'Brandstof', 'Vermogen', 'Bouwjaar', 'BPM','CO2 uitstoot gecombineerd', 'Cilinderinhoud','Massa leeg voertuig']                                    
 
 
-def get_html(base_link):
-    url_request = urllib.request.Request(base_link, headers = {'User-Agent': 'Mozilla/5.0'})
+def get_html(full_link): 
+    """
+    Requests and opens chosen link for reading.
+    """
+
+    url_request = urllib.request.Request(full_link, headers = {'User-Agent': 'Mozilla/5.0'})
     url_html = urllib.request.urlopen(url_request).read()
     return url_html
 
 
-def get_info(base_link):
-    soup = BeautifulSoup(get_html(base_link),'html.parser')
+def get_info(full_link):
+    """
+    Returns value of keywords
+    """
+    soup = BeautifulSoup(get_html(full_link),'html.parser')
     all_tds = soup.find_all('td')
     keyword_index = []
     kenteken_data = []
-    for i, x in enumerate(all_tds):
+    
+    #Finds the position of the keyword in the list of 'td'. 
+    for i, x in enumerate(all_tds):            
         if x.string in keywords:
             keyword_index.append(i)
+    
+    #Finds the value of the keyword in the list of 'td'        
     for y in keyword_index:
         kenteken_data.append(all_tds[y+2].string)
     return list(OrderedSet(kenteken_data))
 
 
-def write_to_csv(a_link):
+def write_to_csv(full_link):
     try:
         with open('kenteken_data_test.csv', 'a') as f:
             writer = csv.writer(f)
-            writer.writerow(get_info(a_link))
+            writer.writerow(get_info(full_link))
     except FileNotFoundError:
-        print('ERRROOORRRRR REEEEE')
+        print("File not found")
     finally:
         f.close()
 
@@ -41,20 +53,20 @@ def write_to_csv(a_link):
 def main():
     base_link = 'https://www.rdwdata.nl/kenteken/'  
 
-    fname = input('Enter filename for number plates')
+    file_name = input('Enter filename for number plates: ')
     
-    with open(fname) as f:
+    with open(file_name) as f:
         for line in f:
-            a_link = base_link + line
-            print(a_link)
-            if a_link.strip() != base_link.strip():
+            full_link = base_link + line
+            print(full_link)
+            if full_link.strip() != base_link.strip():
                 try:
-                    write_to_csv(a_link)
+                    write_to_csv(full_link)
                 except urllib.error.HTTPError:
-                    print("bruuuuuuuhhhhhhh")
+                    print("Link not found")
 
             else:
-                print("Looks empty")
+                print("Empty line")
                 break
     
           
